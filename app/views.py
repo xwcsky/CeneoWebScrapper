b3ddee34c6352d914f1ff7b68eafe8614cc55de9
Wiktor Jurkowski <wicio100@interia.pl>
2025-05-29 17:10:29 +0200

    29.05.2025 WJ

--- app/views.py
+++ app/views.py
@@ -7,26 +7,26 @@ from bs4 import BeautifulSoup
 from flask import render_template, request, redirect, url_for
 from config import headers
 from app import utils
- 
+
 @app.route("/")
 def index():
     return render_template("index.html")
- 
+
 @app.route("/extract")
 def display_form():
     return render_template("extract.html")
- 
+
 @app.route("/extract", methods=["POST"])
 def extract():
     product_id = request.form.get('product_id')
     next_page = f"https://www.ceneo.pl/{product_id}#tab=reviews"
     response = requests.get(next_page, headers=headers)
     if response.status_code == 200:
-        page_dom = BeautifulSoup(response.text, "html.parser") 
-        product_name = utils.extract_feature(page_dom, "h1" )
-        opinions_count = utils.extract_feature(page_dom, "a.product-review__link > spam")
+        page_dom = BeautifulSoup(response.text, "html.parser")
+        product_name = utils.extract_feature(page_dom, "h1")
+        opinions_count = utils.extract_feature(page_dom, "a.product-review__link > span")
         if not opinions_count:
-            error="Dla produktu o podanym id nie ma jeszcze zadnych opinii"
+            error="Dla produktu o podanym id nie ma jeszcze żadnych opinii"
             return render_template("extract.html", error=error)
     else:
         error="Nie znaleziono produktu o podanym id"
@@ -56,17 +56,17 @@ def extract():
     with open(f"./app/data/opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
         json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
     return redirect(url_for('product', product_id=product_id, product_name=product_name))
- 
+
 @app.route("/products")
 def products():
     return render_template("products.html")
- 
+
 @app.route("/author")
 def author():
     return render_template("author.html")
- 
+
 @app.route("/product/<product_id>")
 def product(product_id):
-    product_name = request.args.get("product_name")
-    opinions = pd.read__json(f"./app/data/opinions/{product_id}.json")
-    return render_template("product.html", product_id=product_id, product_name = product_name, opinions = opinions.to_html(table_id="opinions", classes=""))
+    product_name=request.args.get('product_name')
+    opinions = pd.read_json(f"./app/data/opinions/{product_id}.json")
+    return render_template("product.html", product_id=product_id, product_name=product_name, opinions=opinions.to_html(table_id='opinions', classes=['display']))
